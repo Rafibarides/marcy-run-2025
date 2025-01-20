d1c323b58eb37cb43136dcb3395db75d8180c555
Rafibarides <rafibaridesmusic@gmail.com>
2025-01-19 23:58:13 -0500

    deploy URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/marcy-run-2025/',
+  base: '/',
   build: {
     outDir: 'dist',
   }
